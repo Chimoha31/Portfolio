983099e8ed077cf6d9a0cf4580ab8f605d5e096c
Chimoha31 <mokochii1108@gmail.com>
2022-03-26 19:46:42 -0700

    Added education

--- src/timelineElements.js
+++ src/timelineElements.js
@@ -1,42 +1,42 @@
 let timelineElements = [
   {
     id: 1,
-    title: "CICCC Student",
+    title: "Cornerstone International Community College Of Canada",
     location: "Vancouver, BC",
-    description: "Web application Diploma. gfdulsacnfiasbvui;abvia;njfai;",
+    description: "Web And Mobile App Development Diploma",
     date: "August 2021 - present",
     icon: "study"
   },
   {
     id: 2,
-    title: "CICCC Student",
+    title: "Arbutus College",
     location: "Vancouver, BC",
-    description: "Web application Diploma. gfdulsacnfiasbvui;abvia;njfai;",
-    date: "August 2021 - present",
+    description: "Honours Co-op Diploma in Business with Specializations in Translation & Interpreting",
+    date: "May 2019 - June 2021",
     icon: "study"
   },
   {
     id: 3,
-    title: "CICCC Student",
-    location: "Vancouver, BC",
-    description: "Web application Diploma. gfdulsacnfiasbvui;abvia;njfai;",
-    date: "August 2021 - present",
+    title: "Arts Trading Co., Ltd (An Import Trading Company)",
+    location: "Tokyo, Japan",
+    description: "",
+    date: "April 2014 - April 2019",
     icon: "work"
   },
   {
     id: 4,
-    title: "CICCC Student",
-    location: "Vancouver, BC",
-    description: "Web application Diploma. gfdulsacnfiasbvui;abvia;njfai;",
-    date: "August 2021 - present",
+    title: "Central Washington University",
+    location: "Washington, U.S.A",
+    description: "Asia University America Program / Study abroad program",
+    date: "September 2010 - Febrary 2011",
     icon: "study"
   },
   {
     id: 5,
-    title: "CICCC Student",
-    location: "Vancouver, BC",
-    description: "Web application Diploma. gfdulsacnfiasbvui;abvia;njfai;",
-    date: "August 2021 - present",
+    title: "Asia University",
+    location: "Tokyo, Japan",
+    description: "Bachelor's degree in International relations",
+    date: "April 2010 - March 2014",
     icon: "study"
   },
 ]
