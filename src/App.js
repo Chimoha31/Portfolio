0739636f7e708b796d20a1cbe4a3fdf130ca009c
Chimoha31 <mokochii1108@gmail.com>
2022-04-01 16:11:17 -0700

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import AboutMe from './components/AboutMe/AboutMe';
-import Home from './components/Home/Home';
-import Works from './components/Works/Works';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+import AboutMe from "./components/AboutMe/AboutMe";
+import Home from "./components/Home/Home";
+import Works from "./components/Works/Works";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Router>
-      <NavBar />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/aboutme" element={<AboutMe />}></Route>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css"
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact_container">
+      <div className="contact_left">
+        <ul>
+          <li>
+            <i className="fas fa-envelope-square"></i>
+            <p>mokochii1108@gmail.com</p>
+          </li>
+          <li>
+            <i className="fas fa-mobile-alt"></i>
+            <p>6047852569</p>
+          </li>
+        </ul>
+      </div>
+      <div className="contact_right">
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
