8d20748e70258969ed94cbdeb77254d7497a549b
Chimoha31 <mokochii1108@gmail.com>
2022-04-04 12:27:33 -0700

    Finally fixed bug...

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -3,30 +3,25 @@ import wetherApp from "../../images/weatherApp.png";
 import "./Works.css";
 import html from "../../images/html.png";
 import css from "../../images/css.png";
-import sass from "../../images/sass.png";
-import js from "../../images/js.png";
-import react from "../../images/react.png";
-import typescript from "../../images/typescript.png";
+// import sass from "../../images/sass.png";
+// import js from "../../images/js.png";
+// import react from "../../images/react.png";
+// import typescript from "../../images/typescript.png";
 import projectsElements from "../../projectsElements";
 
 function Works() {
   return (
     <div className="works_container">
       {projectsElements.map((project) => (
-        <div className="works_card">
+        <div className="works_card" key={project.id}>
           <img src={wetherApp} className="card_pic" alt="card" />
           <h2 className="card_title">{project.title}</h2>
-          <p className="card_description">
-            {project.description}
-          </p>
+          <p className="card_description">{project.description}</p>
           <h5>-Techonologies-</h5>
           <div className="tech_icons">
-            <img src={css} alt="css" />
-            <img src={html} alt="html" />
-            <img src={js} alt="js" />
-            <img src={typescript} alt="ts" />
-            <img src={sass} alt="sass" />
-            <img src={react} alt="react" />
+            {project.technology.map((tech) => (
+              <img src={tech} alt="icons" />
+            ))}
           </div>
           <div className="button">
             <a href="#" className="github">
--- src/projectsElements.js
+++ src/projectsElements.js
@@ -1,34 +1,53 @@
-let projectElements = [
+import html from "./images/html.png";
+import css from "./images/css.png";
+import sass from "./images/sass.png";
+import js from "./images/js.png";
+import react from "./images/react.png";
+import typescript from "./images/typescript.png";
+
+let projectsElements = [
   {
     id: 1,
     title: "Title 1",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css, sass],
   },
   {
     id: 2,
     title: "Title 2",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css],
   },
   {
     id: 3,
     title: "Title 3",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css],
   },
   {
     id: 4,
     title: "Title 4",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css],
   },
   {
     id: 5,
     title: "Title 5",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css],
   },
   {
     id: 6,
     title: "Title 6",
-    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
-  }
+    description:
+      "This is App description. Lorem ipsum dolor sit amet consectetur",
+    technology: [html, css],
+  },
 ];
 
-export default projectElements;
+export default projectsElements;
