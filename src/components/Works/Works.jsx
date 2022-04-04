58edf7df3158c329902c526defd7318bc6f11d1c
Chimoha31 <mokochii1108@gmail.com>
2022-04-03 23:58:13 -0700

    Created projectsElements

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,79 +1,43 @@
 import React from "react";
 import wetherApp from "../../images/weatherApp.png";
 import "./Works.css";
-import html from '../../images/html.png';
-import css from '../../images/css.png';
-import sass from '../../images/sass.png';
-import js from '../../images/js.png';
-import react from '../../images/react.png';
-import typescript from '../../images/typescript.png';
+import html from "../../images/html.png";
+import css from "../../images/css.png";
+import sass from "../../images/sass.png";
+import js from "../../images/js.png";
+import react from "../../images/react.png";
+import typescript from "../../images/typescript.png";
+import projectsElements from "../../projectsElements";
 
 function Works() {
   return (
     <div className="works_container">
-      <div className="works_card">
-        <img src={wetherApp} className="card_pic" alt="card" />
-        <h2 className="card_title">This is App Title</h2>
-        <p className="card_description">
-          This is App description. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit.
-        </p>
-        <h5>-Techonologies-</h5>
-        <div className="tech_icons">
-          <img src={css} alt="css"/>
-          <img src={html} alt="html"/>
-          <img src={js} alt="js"/>
-          <img src={typescript} alt="ts"/>
-          <img src={sass} alt="sass"/>
-          <img src={react} alt="react"/>
+      {projectsElements.map((project) => (
+        <div className="works_card">
+          <img src={wetherApp} className="card_pic" alt="card" />
+          <h2 className="card_title">{project.title}</h2>
+          <p className="card_description">
+            {project.description}
+          </p>
+          <h5>-Techonologies-</h5>
+          <div className="tech_icons">
+            <img src={css} alt="css" />
+            <img src={html} alt="html" />
+            <img src={js} alt="js" />
+            <img src={typescript} alt="ts" />
+            <img src={sass} alt="sass" />
+            <img src={react} alt="react" />
+          </div>
+          <div className="button">
+            <a href="#" className="github">
+              github
+            </a>
+            <a href="#" className="app">
+              App
+            </a>
+          </div>
         </div>
-        <div className="button">
-          <a href="#" className="github">github</a>
-          <a href="#" className="app">App</a>
-        </div>
-      </div>
-      <div className="works_card">
-        <img src={wetherApp} className="card_pic" alt="card" />
-        <h2 className="card_title">This is App Title</h2>
-        <p className="card_description">
-          This is App description. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit.
-        </p>
-        <h5>-Techonologies-</h5>
-        <div className="tech_icons">
-          <img src={css} alt="css"/>
-          <img src={html} alt="html"/>
-          <img src={js} alt="js"/>
-          <img src={typescript} alt="ts"/>
-          <img src={sass} alt="sass"/>
-          <img src={react} alt="react"/>
-        </div>
-        <div className="button">
-          <a href="#" className="github">github</a>
-          <a href="#" className="app">App</a>
-        </div>
-      </div>
-      <div className="works_card">
-        <img src={wetherApp} className="card_pic" alt="card" />
-        <h2 className="card_title">This is App Title</h2>
-        <p className="card_description">
-          This is App description. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit.
-        </p>
-        <h5>-Techonologies-</h5>
-        <div className="tech_icons">
-          <img src={css} alt="css"/>
-          <img src={html} alt="html"/>
-          <img src={js} alt="js"/>
-          <img src={typescript} alt="ts"/>
-          <img src={sass} alt="sass"/>
-          <img src={react} alt="react"/>
-        </div>
-        <div className="button">
-          <a href="#" className="github">github</a>
-          <a href="#" className="app">App</a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
---
+++ src/projectsElements.js
@@ -0,0 +1,34 @@
+let projectElements = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    id: 4,
+    title: "Title 4",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    id: 5,
+    title: "Title 5",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    id: 6,
+    title: "Title 6",
+    description: "This is App description. Lorem ipsum dolor sit amet consectetur",
+  }
+];
+
+export default projectElements;
