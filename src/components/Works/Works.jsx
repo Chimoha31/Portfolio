92501dd20b76df4f8ac36d2e98814f7874b3ea6c
Chimoha31 <mokochii1108@gmail.com>
2022-05-06 20:15:02 -0700

    Changed icon to text

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -7,14 +7,16 @@ function Works() {
     <div className="works_container">
       {projectsElements.map((project) => (
         <div className="works_card" key={project.id}>
-            <img src={project.image} className="card_pic" alt="card" />
+          <img src={project.image} className="card_pic" alt="card" />
           <h2 className="card_title">{project.title}</h2>
           <p className="card_description">{project.description}</p>
           <h5>-Techonologies-</h5>
           <div className="tech_icons">
-            {project.technology.map((tech, index) => (
-              <img src={tech} alt="icons" key={index} />
-            ))}
+            <div className="tech_lang">
+              {project.technology.map((tech, index) => (
+                <p key={index}>{tech}</p>
+              ))}
+            </div>
           </div>
           <div className="button">
             <a href={project.github} className="github">
--- src/projectsElements.js
+++ src/projectsElements.js
@@ -1,25 +1,7 @@
-import css from "./images/css.png";
-import sass from "./images/sass.png";
-import bootstrap from "./images/bootstrap.png";
-import materialui from './images/materialui.png';
-import js from "./images/js.png";
-import jquery from './images/jquery.png';
-import react from "./images/react.png";
-import typescript from "./images/typescript.png";
-import nodejs from './images/nodejs.png';
-import postman from './images/postman.png';
-import mongodb from './images/mongodb.png';
-import stripe from './images/stripe.png';
-import firebase from './images/firebase.png';
-import vercel from './images/vercel.png';
-import heroku from './images/heroku.png';
-import netlify from './images/netlify.png';
-import git from './images/git.png';
-import github from './images/github.png';
 import weatherApp from "./images/weatherApp.png";
-import recipe_app from './projectsImage/recipe_app.png';
-import rock_paper_scissors from './projectsImage/rock_paper_scissors.png';
-import anime_database_app from './projectsImage/anime_database_app.png';
+import recipe_app from "./projectsImage/recipe_app.png";
+import rock_paper_scissors from "./projectsImage/rock_paper_scissors.png";
+import anime_database_app from "./projectsImage/anime_database_app.png";
 
 let projectsElements = [
   {
@@ -28,7 +10,7 @@ let projectsElements = [
     title: "Ezy Pzy Recipe",
     description:
       "This is a foods recipe application by using API for who want to know the ingredients for meals or how to make",
-    technology: [css, jquery, react, materialui, netlify],
+    technology: ["css", "jquery", "react", "materialui", "netlify"],
     github: "https://github.com/Chimoha31/Recipe.git",
     app: "https://ezypzy-recipe.netlify.app",
   },
@@ -36,11 +18,10 @@ let projectsElements = [
     id: 2,
     image: rock_paper_scissors,
     title: "Rock Paper Scissors",
-    description:
-      "This is a game App for fun, ROCK PAPER SCISSORS",
-    technology: [sass, react, vercel],
+    description: "This is a game App for fun, ROCK PAPER SCISSORS",
+    technology: ["sass", "react", "vercel"],
     github: "https://github.com/Chimoha31/Rock_Paper_Scissors.git",
-    app: "https://rock-paper-scissors-fungame.vercel.app/"
+    app: "https://rock-paper-scissors-fungame.vercel.app/",
   },
   {
     id: 3,
@@ -48,9 +29,16 @@ let projectsElements = [
     title: "Anime Database",
     description:
       "This is an Anime Database application. You can see top, airing, upcoming, o.v.a anime in Japan. Also, you can search anime what you want to know the anime series. Moreover, you can put your favorite anime in your list",
-    technology: [css, bootstrap,materialui, react, firebase, vercel],
+    technology: [
+      "css",
+      "bootstrap",
+      "material ui",
+      "react",
+      "firebase",
+      "vercel",
+    ],
     github: "https://github.com/Chimoha31/Anime_data.git",
-    app: ""
+    app: "",
   },
   {
     id: 4,
@@ -58,7 +46,7 @@ let projectsElements = [
     title: "Title 4",
     description:
       "This is App description. Lorem ipsum dolor sit amet consectetur",
-    technology: [css, sass],
+    technology: ["sass", "react", "vercel"],
   },
   {
     id: 5,
@@ -66,7 +54,7 @@ let projectsElements = [
     title: "Title 5",
     description:
       "This is App description. Lorem ipsum dolor sit amet consectetur",
-    technology: [css],
+    technology: ["sass", "react", "vercel"],
   },
   {
     id: 6,
@@ -74,7 +62,7 @@ let projectsElements = [
     title: "Title 6",
     description:
       "This is App description. Lorem ipsum dolor sit amet consectetur",
-    technology: [css],
+    technology: ["sass", "react", "vercel"],
   },
 ];
 
