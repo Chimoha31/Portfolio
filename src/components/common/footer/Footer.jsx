1dcbf3f2051160040f4107d769d0fc12a9969bee
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-26 16:51:17 -0800

    DONE

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <i data-aos="zoom-in-left" className="fa-brands fa-github"></i>
-        <i data-aos="zoom-in-left" className="fa-brands fa-linkedin"></i>
-        <p data-aos="zoom-in-righht">©2023 - Chiho Maekawa / All Rights Reserved</p>
+        <i
+          data-aos="zoom-in-left"
+          className="fa-brands fa-github"
+          onClick={() =>
+            (window.location.href = "https://github.com/Chimoha31")
+          }
+        ></i>
+        <i
+          data-aos="zoom-in-left"
+          className="fa-brands fa-linkedin"
+          onClick={() =>
+            (window.location.href =
+              "https://www.linkedin.com/in/chiho1108developer/")
+          }
+        ></i>
+        <p data-aos="zoom-in-righht">
+          ©2023 - Chiho Maekawa / All Rights Reserved
+        </p>
       </footer>
     </>
   );
